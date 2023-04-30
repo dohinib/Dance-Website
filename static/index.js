9adbd7309df73a2d8e9c6061ffec94f1ddb595e4
dohinib <108190147+dohinib@users.noreply.github.com>
2023-04-30 23:18:06 +0530

    Update index.js

---
+++ static/index.js
@@ -0,0 +1,17 @@
+// [index.js] with server app export.
+
+const express = require("express");
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.get('/hello-world', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+// Export the Express API
+module.exports = app;
